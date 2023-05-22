ba3d24a3e34dec8ea13b7150cf154595ed9c26ce
George McLachlan <georgemclachlan2@gmail.com>
2023-05-22 20:11:02 +1200

    Implement wheel size scaling

--- Private/VehiclePreview.cpp
+++ Private/VehiclePreview.cpp
@@ -79,14 +79,18 @@ void AVehiclePreview::Tick(float DeltaTime)
 
 void AVehiclePreview::SetWheelOffsets(FVehicleType* VehicleType)
 {
+	FVector const WheelScalar = FVector(VehicleType->WheelSizeModifier, VehicleType->WheelSizeModifier, VehicleType->WheelSizeModifier);
+	
 	FrontLeftRim->SetRelativeLocation(FVector(VehicleType->FrontAxelOffset, -VehicleType->AxelLength, 40.f));
-	FrontLeftRim->SetRelativeScale3D(FVector(1.f, -1.f, 1.f));
+	FrontLeftRim->SetRelativeScale3D(WheelScalar);
 	
 	FrontRightRim->SetRelativeLocation(FVector(VehicleType->FrontAxelOffset, VehicleType->AxelLength, 40.f));
+	FrontRightRim->SetRelativeScale3D(WheelScalar);
 
 	RearLeftRim->SetRelativeLocation(FVector(VehicleType->RearAxelOffset, -VehicleType->AxelLength, 40.f));
-	RearLeftRim->SetRelativeScale3D(FVector(1.f, -1.f, 1.f));
+	RearLeftRim->SetRelativeScale3D(WheelScalar);
 	
 	RearRightRim->SetRelativeLocation(FVector(VehicleType->RearAxelOffset, VehicleType->AxelLength, 40.f));
+	RearRightRim->SetRelativeScale3D(WheelScalar);
 }
 
