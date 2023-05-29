6c7383a9a6fecc26037a43dee55e5d3031ea7caa
Georgefwm <georgemclachlan2@gmail.com>
2023-05-29 16:42:11 +1200

    Add Session class

---
+++ Private/Framework/PolyRacingSessionSubsystem.cpp
@@ -0,0 +1,273 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OnlineSubsystemUtils.h"
+#include "Framework/PolyRacingSessionSubsystem.h"
+
+UPolyRacingSessionSubsystem::UPolyRacingSessionSubsystem()
+	: CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionCompleted))
+{
+}
+
+void UPolyRacingSessionSubsystem::CreateSession(int32 NumPublicConnections, bool IsLANMatch)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnCreateSessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	LastSessionSettings = MakeShareable(new FOnlineSessionSettings());
+	LastSessionSettings->NumPrivateConnections = 0;
+	LastSessionSettings->NumPublicConnections = NumPublicConnections;
+	LastSessionSettings->bAllowInvites = true;
+	LastSessionSettings->bAllowJoinInProgress = true;
+	LastSessionSettings->bAllowJoinViaPresence = true;
+	LastSessionSettings->bAllowJoinViaPresenceFriendsOnly = true;
+	LastSessionSettings->bIsDedicated = false;
+	LastSessionSettings->bUsesPresence = true;
+	LastSessionSettings->bIsLANMatch = IsLANMatch;
+	LastSessionSettings->bShouldAdvertise = true;
+
+	LastSessionSettings->Set(SETTING_MAPNAME, FString("%LEVEL_NAME%"), EOnlineDataAdvertisementType::ViaOnlineService);
+
+	CreateSessionCompleteDelegateHandle = SessionInterface->AddOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegate);
+
+	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	if (!SessionInterface->CreateSession(*LocalPlayer->GetPreferredUniqueNetId(), NAME_GameSession, *LastSessionSettings))
+	{
+		SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
+
+		OnCreateSessionCompleteEvent.Broadcast(false);
+	}
+}
+
+void UPolyRacingSessionSubsystem::UpdateSession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnUpdateSessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	TSharedPtr<FOnlineSessionSettings> UpdatedSessionSettings = MakeShareable(new FOnlineSessionSettings(*LastSessionSettings));
+
+	// Make session changes/updates
+	UpdatedSessionSettings->Set(SETTING_MAPNAME, FString("%UPDATED_LEVEL_NAME%"), EOnlineDataAdvertisementType::ViaOnlineService);
+
+	UpdateSessionCompleteDelegateHandle = SessionInterface->AddOnUpdateSessionCompleteDelegate_Handle(UpdateSessionCompleteDelegate);
+
+	if (!SessionInterface->UpdateSession(NAME_GameSession, *UpdatedSessionSettings))
+	{
+		SessionInterface->ClearOnUpdateSessionCompleteDelegate_Handle(UpdateSessionCompleteDelegateHandle);
+
+		OnUpdateSessionCompleteEvent.Broadcast(false);
+	}
+	else
+	{
+		LastSessionSettings = UpdatedSessionSettings;
+	}
+}
+
+void UPolyRacingSessionSubsystem::StartSession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnStartSessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	StartSessionCompleteDelegateHandle = SessionInterface->AddOnStartSessionCompleteDelegate_Handle(StartSessionCompleteDelegate);
+
+	if (!SessionInterface->StartSession(NAME_GameSession))
+	{
+		SessionInterface->ClearOnStartSessionCompleteDelegate_Handle(StartSessionCompleteDelegateHandle);
+
+		OnStartSessionCompleteEvent.Broadcast(false);
+	}
+}
+
+void UPolyRacingSessionSubsystem::EndSession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnEndSessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	EndSessionCompleteDelegateHandle = SessionInterface->AddOnEndSessionCompleteDelegate_Handle(EndSessionCompleteDelegate);
+
+	if (!SessionInterface->EndSession(NAME_GameSession))
+	{
+		SessionInterface->ClearOnEndSessionCompleteDelegate_Handle(EndSessionCompleteDelegateHandle);
+
+		OnEndSessionCompleteEvent.Broadcast(false);
+	}
+}
+
+void UPolyRacingSessionSubsystem::DestroySession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnDestroySessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	DestroySessionCompleteDelegateHandle = SessionInterface->AddOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegate);
+
+	if (!SessionInterface->DestroySession(NAME_GameSession))
+	{
+		SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegateHandle);
+
+		OnDestroySessionCompleteEvent.Broadcast(false);
+	}
+}
+
+void UPolyRacingSessionSubsystem::FindSessions(int32 MaxSearchResults, bool IsLANQuery)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnFindSessionsCompleteEvent.Broadcast(TArray<FOnlineSessionSearchResult>(), false);
+		return;
+	}
+
+	FindSessionsCompleteDelegateHandle = SessionInterface->AddOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegate);
+
+	LastSessionSearch = MakeShareable(new FOnlineSessionSearch());
+	LastSessionSearch->MaxSearchResults = MaxSearchResults;
+	LastSessionSearch->bIsLanQuery = IsLANQuery;
+
+	// Set search params
+	LastSessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+
+	
+	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	if (!SessionInterface->FindSessions(*LocalPlayer->GetPreferredUniqueNetId(), LastSessionSearch.ToSharedRef()))
+	{
+		SessionInterface->ClearOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegateHandle);
+
+		OnFindSessionsCompleteEvent.Broadcast(TArray<FOnlineSessionSearchResult>(), false);
+	}
+}
+
+void UPolyRacingSessionSubsystem::JoinGameSession(const FOnlineSessionSearchResult& SessionResult)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnJoinGameSessionCompleteEvent.Broadcast(EOnJoinSessionCompleteResult::UnknownError);
+		return;
+	}
+
+	JoinSessionCompleteDelegateHandle =
+		SessionInterface->AddOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegate);
+
+	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	if (!SessionInterface->JoinSession(*LocalPlayer->GetPreferredUniqueNetId(), NAME_GameSession, SessionResult))
+	{
+		SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegateHandle);
+
+		OnJoinGameSessionCompleteEvent.Broadcast(EOnJoinSessionCompleteResult::UnknownError);
+	}
+}
+
+void UPolyRacingSessionSubsystem::OnCreateSessionCompleted(FName SessionName, bool Successful)
+{
+	if (const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld()))
+	{
+		SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
+	}
+
+	OnCreateSessionCompleteEvent.Broadcast(Successful);
+}
+
+void UPolyRacingSessionSubsystem::OnUpdateSessionCompleted(FName SessionName, bool Successful)
+{
+	if (const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld()))
+	{
+		SessionInterface->ClearOnUpdateSessionCompleteDelegate_Handle(UpdateSessionCompleteDelegateHandle);
+	}
+
+	OnUpdateSessionCompleteEvent.Broadcast(Successful);
+}
+
+void UPolyRacingSessionSubsystem::OnStartSessionCompleted(FName SessionName, bool Successful)
+{
+	if (const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld()))
+	{
+		SessionInterface->ClearOnStartSessionCompleteDelegate_Handle(StartSessionCompleteDelegateHandle);
+	}
+
+	OnStartSessionCompleteEvent.Broadcast(Successful);
+}
+
+void UPolyRacingSessionSubsystem::OnEndSessionCompleted(FName SessionName, bool Successful)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());;
+	if (SessionInterface)
+	{
+		SessionInterface->ClearOnEndSessionCompleteDelegate_Handle(EndSessionCompleteDelegateHandle);
+	}
+
+	OnEndSessionCompleteEvent.Broadcast(Successful);
+}
+
+void UPolyRacingSessionSubsystem::OnDestroySessionCompleted(FName SessionName, bool Successful)
+{
+	if (const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld()))
+	{
+		SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegateHandle);
+	}
+
+	OnDestroySessionCompleteEvent.Broadcast(Successful);
+}
+
+void UPolyRacingSessionSubsystem::OnFindSessionsCompleted(bool Successful)
+{
+	if (const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld()))
+	{
+		SessionInterface->ClearOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegateHandle);
+	}
+
+	if (LastSessionSearch->SearchResults.Num() <= 0)
+	{
+		OnFindSessionsCompleteEvent.Broadcast(TArray<FOnlineSessionSearchResult>(), Successful);
+		return;
+	}
+
+	OnFindSessionsCompleteEvent.Broadcast(LastSessionSearch->SearchResults, Successful);
+}
+
+void UPolyRacingSessionSubsystem::OnJoinSessionCompleted(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	if (const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld()))
+	{
+		SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegateHandle);
+	}
+
+	OnJoinGameSessionCompleteEvent.Broadcast(Result);
+}
+
+bool UPolyRacingSessionSubsystem::TryTravelToCurrentSession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		return false;
+	}
+
+	FString ConnectString;
+	if (!SessionInterface->GetResolvedConnectString(NAME_GameSession, ConnectString))
+	{
+		return false;
+	}
+
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	PlayerController->ClientTravel(ConnectString, TRAVEL_Absolute);
+	return true;
+}
---
+++ Public/Framework/PolyRacingSessionSubsystem.h
@@ -0,0 +1,87 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "PolyRacingSessionSubsystem.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPROnCreateSessionComplete,		bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPROnUpdateSessionComplete,		bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPROnStartSessionComplete,		bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPROnEndSessionComplete,		bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPROnDestroySessionComplete,	bool, Successful);
+
+DECLARE_MULTICAST_DELEGATE_TwoParams(FPROnFindSessionsComplete, const TArray<FOnlineSessionSearchResult>& SessionResults, bool Successful);
+DECLARE_MULTICAST_DELEGATE_OneParam(FPROnJoinSessionComplete, EOnJoinSessionCompleteResult::Type Result);
+
+/**
+ * 
+ */
+UCLASS()
+class POLYRACINGGAME_API UPolyRacingSessionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UPolyRacingSessionSubsystem();
+
+	void CreateSession(int32 NumPublicConnections, bool IsLANMatch);
+	void UpdateSession();
+	void StartSession();
+	void EndSession();
+	void DestroySession();
+	void FindSessions(int32 MaxSearchResults, bool IsLANQuery);
+	void JoinGameSession(const FOnlineSessionSearchResult& SessionResult);
+
+protected:
+	void OnCreateSessionCompleted(FName SessionName, bool Successful);
+	void OnUpdateSessionCompleted(FName SessionName, bool Successful);
+	void OnStartSessionCompleted(FName SessionName, bool Successful);
+	void OnEndSessionCompleted(FName SessionName, bool Successful);
+	void OnDestroySessionCompleted(FName SessionName, bool Successful);
+	void OnFindSessionsCompleted(bool Successful);
+	void OnJoinSessionCompleted(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	bool TryTravelToCurrentSession();
+
+public:
+	FPROnCreateSessionComplete OnCreateSessionCompleteEvent;
+	FPROnUpdateSessionComplete OnUpdateSessionCompleteEvent;
+	FPROnStartSessionComplete OnStartSessionCompleteEvent;
+	FPROnEndSessionComplete OnEndSessionCompleteEvent;
+	FPROnDestroySessionComplete OnDestroySessionCompleteEvent;
+	FPROnFindSessionsComplete OnFindSessionsCompleteEvent;
+	FPROnJoinSessionComplete OnJoinGameSessionCompleteEvent;
+
+	TSharedPtr<FOnlineSessionSettings> LastSessionSettings;
+	TSharedPtr<FOnlineSessionSearch> LastSessionSearch;
+
+	// Create session
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+
+	// Update session
+	FOnUpdateSessionCompleteDelegate UpdateSessionCompleteDelegate;
+	FDelegateHandle UpdateSessionCompleteDelegateHandle;
+
+	// Start session
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
+
+	// End session
+	FOnEndSessionCompleteDelegate EndSessionCompleteDelegate;
+	FDelegateHandle EndSessionCompleteDelegateHandle;
+
+	// Destroy session
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+
+	// Find session
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FDelegateHandle FindSessionsCompleteDelegateHandle;
+
+	// Join session
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+};
