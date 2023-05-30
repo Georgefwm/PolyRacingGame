035579759c68716db045e7f8bb4423a6eec357c5
Georgefwm <georgemclachlan2@gmail.com>
2023-05-31 11:11:09 +1200

    Add multiplayer menu

---
+++ Private/UI/Menu/MultiplayerMenuWidget.cpp
@@ -0,0 +1,133 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Menu/MultiplayerMenuWidget.h"
+
+#include "Framework/PolyRacingSessionSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/Style/GlobalMenuStyle.h"
+#include "UI/Menu/MainMenuWidget.h"
+#include "UI/MenuHUD.h"
+#include "UI/Style/UiStyles.h"
+
+#define LOCTEXT_NAMESPACE "multiplayermenu"
+
+void SMultiplayerMenuWidget::Construct(const FArguments& InArgs)
+{
+	bCanSupportFocus = true;
+
+	OwningHUD = InArgs._OwningHUD;
+	Style = &FUiStyles::Get().GetWidgetStyle<FGlobalStyle>("PolyRacingMenuStyle");
+	
+
+	/** Text */
+	const FText TitleText		= LOCTEXT("menu title", "Single Player");
+	
+	const FText RaceModeText	= LOCTEXT("RaceModeText", "Race");
+	const FText FreeModeText	= LOCTEXT("FreeModeText", "Free Roam");
+	
+	const FText BackText		= LOCTEXT("Back text", "Back");
+
+	
+	ChildSlot
+	[
+		SNew(SOverlay)
+		+ SOverlay::Slot()
+		.HAlign(HAlign_Left)
+		.VAlign(VAlign_Center)
+		.Padding(Style->TitleTextMargin)
+		[
+			// TitleText
+			SNew(STextBlock)
+			.TextStyle(&Style->MenuTitleStyle)
+			.Text(TitleText)
+			.LineHeightPercentage(2.f)
+		]
+		
+		+ SOverlay::Slot()
+		.HAlign(HAlign_Left)
+		.VAlign(VAlign_Center)
+		.Padding(Style->MenuBoxMargin)
+		[
+			SNew(SVerticalBox)
+			+ SVerticalBox::Slot()
+			.Padding(Style->MenuButtonSpacingMargin)
+			[
+				SNew(SButton)
+				.ButtonStyle(&Style->MenuButtonStyle)
+				.TextStyle(&Style->MenuButtonTextStyle)
+				.Text(RaceModeText)
+				.OnClicked(this, &SMultiplayerMenuWidget::OnRaceClicked)
+			]
+
+			+ SVerticalBox::Slot()
+			.Padding(Style->MenuButtonSpacingMargin)
+			[
+				SNew(SButton)
+				.ButtonStyle(&Style->MenuButtonStyle)
+				.TextStyle(&Style->MenuButtonTextStyle)
+				.Text(FreeModeText)
+				.OnClicked(this, &SMultiplayerMenuWidget::OnFreeRoamClicked)
+			]
+		]
+		
+		// Back Button
+		+ SOverlay::Slot()
+		.HAlign(HAlign_Left)
+		.VAlign(VAlign_Bottom)
+		.Padding(Style->MenuActionButtonContainerMargin)
+		[
+			SNew(SBox)
+			.WidthOverride(1000.f)
+			[
+				SNew(SHorizontalBox)
+				+ SHorizontalBox::Slot()
+				.Padding(Style->MenuActionButtonSpacingMargin)
+				.AutoWidth()
+				[
+					SNew(SBox)
+					.HAlign(HAlign_Left)
+					[
+						SNew(SButton)
+						.ButtonStyle(&Style->MenuActionButtonStyle)
+						.TextStyle(&Style->MenuActionButtonTextStyle)
+						.Text(BackText)
+						.OnClicked(this, &SMultiplayerMenuWidget::OnBackClicked)
+					]
+				]				
+			]			
+		]
+	];
+}
+
+FReply SMultiplayerMenuWidget::OnRaceClicked() const
+{
+
+	return FReply::Handled();
+}
+
+FReply SMultiplayerMenuWidget::OnFreeRoamClicked() const
+{
+	UPolyRacingSessionSubsystem* SessionSubsystem = OwningHUD->GetGameInstance()->GetSubsystem<UPolyRacingSessionSubsystem>();
+	SessionSubsystem->CreateSession(6, false);
+	SessionSubsystem->StartSession();
+	
+	FString const LevelOptions = FString(TEXT("listen -game=/Game/GameModes/BP_FreeRoamGamemode.BP_FreeRoamGamemode_C"));
+	UGameplayStatics::OpenLevel(OwningHUD->GetWorld(), "/Game/Scenes/Docks", true, LevelOptions);
+
+	
+	
+	return FReply::Handled();
+}
+
+FReply SMultiplayerMenuWidget::OnBackClicked() const
+{
+	if (!OwningHUD->MainMenuWidget)
+		OwningHUD->MainMenuWidget = SNew(SMainMenuWidget).OwningHUD(OwningHUD);
+	
+	OwningHUD->MenuWidgetContainer.Get()->SetContent(OwningHUD->MainMenuWidget.ToSharedRef());
+	
+	return FReply::Handled();
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Public/UI/Menu/MultiplayerMenuWidget.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "SlateBasics.h"
+#include "SlateExtras.h"
+#include "Customisation/VehicleCustomiser.h"
+
+
+/**
+ * 
+ */
+class SMultiplayerMenuWidget : public SCompoundWidget
+{
+public:
+
+	SLATE_BEGIN_ARGS(SMultiplayerMenuWidget) {}
+
+	SLATE_ARGUMENT(TWeakObjectPtr<class AMenuHUD>, OwningHUD)
+
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs);
+
+	FReply OnRaceClicked() const;
+	FReply OnFreeRoamClicked() const;
+	FReply OnBackClicked() const;
+
+	TWeakObjectPtr<class AMenuHUD> OwningHUD;
+	const struct FGlobalStyle* Style;
+
+	virtual bool SupportsKeyboardFocus() const override { return true; }
+};
