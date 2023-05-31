18c3afe587da4366c574bc9c7e2baa57c83873e2
Georgefwm <georgemclachlan2@gmail.com>
2023-05-31 12:35:34 +1200

    Add GameInstance class for level loading events

---
+++ Private/Framework/PolyRacingGameInstance.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Framework/PolyRacingGameInstance.h"
+
+void UPolyRacingGameInstance::Init()
+{
+	Super::Init();
+
+	FCoreUObjectDelegates::PreLoadMap.AddUObject(this, &UPolyRacingGameInstance::BeginLoadingScreen);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &UPolyRacingGameInstance::EndLoadingScreen);
+}
+
+void UPolyRacingGameInstance::EndLoadingScreen(UWorld* InLoadedWorld)
+{
+	// if (!IsRunningDedicatedServer())
+	// {
+	// 	FLoadingScreenAttributes LoadingScreen;
+	// 	LoadingScreen.bAutoCompleteWhenLoadingCompletes = false;
+	// 	LoadingScreen.WidgetLoadingScreen = FLoadingScreenAttributes::NewTestLoadingScreenWidget();
+	//
+	// 	GetMoviePlayer()->SetupLoadingScreen(LoadingScreen);
+	// }
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Yellow, TEXT("MAP LOAD END"));
+}
+
+void UPolyRacingGameInstance::BeginLoadingScreen(const FString& MapName)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Yellow, TEXT("MAP LOAD START"));
+}
---
+++ Public/Framework/PolyRacingGameInstance.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PolyRacingGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POLYRACINGGAME_API UPolyRacingGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+	virtual void Init() override;
+
+	UFUNCTION()
+	virtual void BeginLoadingScreen(const FString& MapName);
+	
+	UFUNCTION()
+	virtual void EndLoadingScreen(UWorld* InLoadedWorld);
+};
