cdf4f7a0c81a1cefc60d6fc3477cb3216072af2d
Georgefwm <georgemclachlan2@gmail.com>
2023-06-01 19:17:56 +1200

    Add player state class

---
+++ Public/Framework/PolyRacingPlayerState.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PolyRacingPlayerState.generated.h"
+
+UCLASS()
+class POLYRACINGGAME_API APolyRacingPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APolyRacingPlayerState();
+
+	bool bIsReady = false;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
