455c065f42d12dce790ccff5733441ed1272950b
Georgefwm <georgemclachlan2@gmail.com>
2023-06-14 15:21:12 +1200

    Add new class for setup of racer start positions

---
+++ Private/StartPostionActor.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StartPositionActor.h"
+
+AStartPositionActor::AStartPositionActor()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	
+}
+
+void AStartPositionActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Public/StartPositionActor.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StartPositionActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POLYRACINGGAME_API AStartPositionActor : public AActor
+{
+	GENERATED_BODY()
+
+	AStartPositionActor();
+
+	virtual void BeginPlay() override;
+};
+
+
+
