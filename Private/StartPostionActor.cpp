3ce60dea4cdcb147a447258f2f25fefbe79dabe9
Georgefwm <georgemclachlan2@gmail.com>
2023-06-14 16:14:54 +1200

    Start on Spawn location actor functionality

--- Private/StartPostionActor.cpp
+++ Private/StartPostionActor.cpp
@@ -2,14 +2,64 @@
 
 
 #include "StartPositionActor.h"
+#include "Kismet/KismetMathLibrary.h"
 
 AStartPositionActor::AStartPositionActor()
 {
 	RootComponent = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+
+	UStaticMeshComponent* Position1 = CreateDefaultSubobject<UStaticMeshComponent>("Position1");
+	UStaticMeshComponent* Position2 = CreateDefaultSubobject<UStaticMeshComponent>("Position2");
+	UStaticMeshComponent* Position3 = CreateDefaultSubobject<UStaticMeshComponent>("Position3");
+	UStaticMeshComponent* Position4 = CreateDefaultSubobject<UStaticMeshComponent>("Position4");
+	UStaticMeshComponent* Position5 = CreateDefaultSubobject<UStaticMeshComponent>("Position5");
+	UStaticMeshComponent* Position6 = CreateDefaultSubobject<UStaticMeshComponent>("Position6");
+	UStaticMeshComponent* Position7 = CreateDefaultSubobject<UStaticMeshComponent>("Position7");
+	UStaticMeshComponent* Position8 = CreateDefaultSubobject<UStaticMeshComponent>("Position8");
 	
+	PreviewMeshes.Add(Position1);
+	PreviewMeshes.Add(Position2);
+	PreviewMeshes.Add(Position3);
+	PreviewMeshes.Add(Position4);
+	PreviewMeshes.Add(Position5);
+	PreviewMeshes.Add(Position6);
+	PreviewMeshes.Add(Position7);
+	PreviewMeshes.Add(Position8);
 }
 
 void AStartPositionActor::BeginPlay()
 {
 	Super::BeginPlay();
+	SetActorHiddenInGame(true);
+}
+
+FTransform AStartPositionActor::GetSpawnTransformFromIndex(int PlayerIndex)
+{
+	return FTransform();
+}
+
+void AStartPositionActor::UpdateEditorPreview()
+{
+	for (int i = 0; i < UKismetMathLibrary::Min(MaxStartLocations, (Shape.X * Shape.Y)); i++)
+	{
+		if (!PreviewMeshes.IsValidIndex(i))
+			return;
+
+		if (!PreviewMesh)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Preview mesh not set"))
+			return;
+		}
+		
+		UStaticMeshComponent* EditingMeshComponent = PreviewMeshes.GetData()[i];
+		EditingMeshComponent->SetStaticMesh(PreviewMesh);
+
+		FVector NewLocation = FVector(
+			1.f,
+			1.f,
+			1.f
+			);
+		
+		EditingMeshComponent->SetRelativeLocation(NewLocation);
+	}
 }
--- Public/StartPositionActor.h
+++ Public/StartPositionActor.h
@@ -13,9 +13,37 @@ class POLYRACINGGAME_API AStartPositionActor : public AActor
 {
 	GENERATED_BODY()
 
+public:
+	
 	AStartPositionActor();
 
 	virtual void BeginPlay() override;
+
+	FTransform GetSpawnTransformFromIndex(int PlayerIndex);
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Editor Preview")
+	TArray<UStaticMeshComponent*> PreviewMeshes;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Editor Preview")
+	UStaticMesh* PreviewMesh;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Start Parameters")
+	bool UseVehicleForPreview = false;
+	
+	UFUNCTION(CallInEditor, Category="Start Parameters")
+	void UpdateEditorPreview();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Start Parameters")
+	int MaxStartLocations = 8;
+
+	// Z will not be used, just feels cleaner to use a vector than two fields
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Start Parameters")
+	FIntVector Shape = FIntVector(2, 4, 0);
+
+	// Again, Z will not be used.
+	// Distance between each starting position
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Start Parameters")
+	FVector Size = FVector(50.f, 50.f, 0.f);
 };
 
 
