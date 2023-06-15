336b936fbf6424da0b284beaa02a3e8d99748ffa
Georgefwm <georgemclachlan2@gmail.com>
2023-06-16 11:51:03 +1200

    Add functions for starting in a new game

--- Private/Controller/PolyRacingPlayerController.cpp
+++ Private/Controller/PolyRacingPlayerController.cpp
@@ -3,6 +3,8 @@
 
 #include "Controller/PolyRacingPlayerController.h"
 
+#include "UI/InGameHUD.h"
+
 APolyRacingPlayerController::APolyRacingPlayerController()
 	: Super()
 {
@@ -14,4 +16,38 @@ void APolyRacingPlayerController::BeginPlay()
 	Super::BeginPlay();
 
 	
+}
+
+void APolyRacingPlayerController::SetupHUD()
+{
+	if (AInGameHUD* HUD = GetHUD<AInGameHUD>())
+	{
+		HUD->ShowPlayerHUD();
+	}
+}
+
+void APolyRacingPlayerController::Client_SetupHUD_Implementation()
+{
+	SetupHUD();
+}
+
+void APolyRacingPlayerController::SpawnVehicleForPlayer(const FPresetVehicleConfiguration& DesiredConfiguration)
+{
+}
+
+void APolyRacingPlayerController::Server_SpawnVehicleForPlayer_Implementation(const FPresetVehicleConfiguration& DesiredConfiguration)
+{
+	SpawnVehicleForPlayer(DesiredConfiguration);
+}
+
+void APolyRacingPlayerController::RequestVehicleSpawn()
+{
+	UVehicleCustomiser* VehicleCustomiser = GetGameInstance()->GetSubsystem<UVehicleCustomiser>();
+	
+	Server_SpawnVehicleForPlayer(VehicleCustomiser->SavedConfigurations->GetData()[VehicleCustomiser->ActiveConfigurationSlotIndex]);
+}
+
+void APolyRacingPlayerController::Client_RequestVehicleSpawn_Implementation()
+{
+	RequestVehicleSpawn();
 }
--- Public/Controller/PolyRacingPlayerController.h
+++ Public/Controller/PolyRacingPlayerController.h
@@ -14,7 +14,28 @@ class POLYRACINGGAME_API APolyRacingPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
+public:
+	
 	APolyRacingPlayerController();
 	
 	virtual void BeginPlay() override;
+
+	void SetupHUD();
+
+	UFUNCTION(Client, Reliable)
+	void Client_SetupHUD();
+	void Client_SetupHUD_Implementation();
+
+	void SpawnVehicleForPlayer(const FPresetVehicleConfiguration& DesiredConfiguration);
+
+	UFUNCTION(Server, Reliable)
+	void Server_SpawnVehicleForPlayer(const FPresetVehicleConfiguration& DesiredConfiguration);
+	void Server_SpawnVehicleForPlayer_Implementation(const FPresetVehicleConfiguration& DesiredConfiguration);
+
+
+	void RequestVehicleSpawn();
+	
+	UFUNCTION(Client, Reliable)
+	void Client_RequestVehicleSpawn();
+	void Client_RequestVehicleSpawn_Implementation();
 };
