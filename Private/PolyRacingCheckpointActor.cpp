069df675ed637aa88caff54025706a8f37b127f5
Georgefwm <georgemclachlan2@gmail.com>
2023-06-19 17:21:07 +1200

    Add Checkpoint class

---
+++ Private/PolyRacingCheckpointActor.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PolyRacingCheckpointActor.h"
+
+#include "Components/BoxComponent.h"
+
+APolyRacingCheckpointActor::APolyRacingCheckpointActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent* SceneRoot = CreateDefaultSubobject<USceneComponent>("SceneRoot");
+	SetRootComponent(SceneRoot);
+
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>("TriggerBox");
+	TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	TriggerBox->SetCollisionResponseToAllChannels(ECR_Overlap);
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &APolyRacingCheckpointActor::OnOverlap);
+}
+
+void APolyRacingCheckpointActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlap happened!"))
+}
+
---
+++ Public/PolyRacingCheckpointActor.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PolyRacingCheckpointActor.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class POLYRACINGGAME_API APolyRacingCheckpointActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	APolyRacingCheckpointActor();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UBoxComponent* TriggerBox;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int CheckpointNumber = 0;
+
+	UFUNCTION()
+	void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+};
