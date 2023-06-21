714431aec4090d78d3d9acc32f1022b009f28896
Georgefwm <georgemclachlan2@gmail.com>
2023-06-21 14:13:37 +1200

    Add checkpoint interface for game modes utilising checkpoints

---
+++ Private/Framework/Gamemode/CheckpointGameMode.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Framework/GameMode/CheckpointGameMode.h"
+
+
+// Add default functionality here for any ICheckpointGameMode functions that are not pure virtual.
+void ICheckpointGameMode::AddCheckpoints(TArray<ACheckpointActor*>& Checkpoints)
+{
+	UE_LOG(LogTemp, Warning, TEXT("AddCheckpoints() Not implemented yet"))
+}
---
+++ Public/Framework/GameMode/CheckpointGameMode.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CheckpointActor.h"
+#include "UObject/Interface.h"
+#include "CheckpointGameMode.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UCheckpointGameMode : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class POLYRACINGGAME_API ICheckpointGameMode
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION()
+	virtual void AddCheckpoints(TArray<ACheckpointActor*>& Checkpoints);
+};
