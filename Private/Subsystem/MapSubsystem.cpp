127e5cc7b12ed92f6dc84b094e81e4a2e3fa3440
Georgefwm <georgemclachlan2@gmail.com>
2023-06-22 14:04:05 +1200

    Add map subsystem

---
+++ Private/Subsystem/MapSubsystem.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystem/MapSubsystem.h"
+
+#include "DataTables.h"
+#include "Engine/DataTable.h"
+
+void UMapSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	FString const MapDataTablePath = TEXT("/Script/Engine.DataTable'/Game/VehicleCustomisation/VehiclePresets.VehiclePresets'");
+	
+	MapDataTable = Cast<UDataTable>(FSoftObjectPath(*MapDataTablePath).ResolveObject());
+	
+	if (!MapDataTable)
+		MapDataTable = Cast<UDataTable>(FSoftObjectPath(*MapDataTablePath).TryLoad());
+
+	if (MapDataTable)
+		UE_LOG(LogTemp, Display, TEXT("UMapSubsystem initialised successfully"))
+}
+
+void UMapSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+FName UMapSubsystem::GetMapPath(FString MapName) const
+{	
+	FMapTableRow* MapTableRow = MapDataTable->FindRow<FMapTableRow>(FName(MapName), "");
+
+	if (!MapTableRow)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Map not found"))
+		return FName("");
+	}
+	
+	return MapTableRow->Path;
+}
---
+++ Public/Subsystem/MapSubsystem.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MapSubsystem.generated.h"
+
+class UDataTable;
+/**
+ * 
+ */
+UCLASS()
+class POLYRACINGGAME_API UMapSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+public:
+
+	UPROPERTY()
+	UDataTable* MapDataTable;
+
+	UPROPERTY()
+	FString CurrentMap;
+
+	FName GetMapPath(FString MapName) const;
+
+	
+};
