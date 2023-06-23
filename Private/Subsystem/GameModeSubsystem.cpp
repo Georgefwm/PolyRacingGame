3276f12619b5c095d8c82f33574191f9b219574c
Georgefwm <georgemclachlan2@gmail.com>
2023-06-23 20:43:21 +1200

    Add subsystem for gamemodes

---
+++ Private/Subsystem/GameModeSubsystem.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystem/GameModeSubsystem.h"
+
+#include "DataTables.h"
+#include "Engine/DataTable.h"
+
+void UGameModeSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	FString const GameModeDataTablePath = TEXT("/Script/Engine.DataTable'/Game/GameModes/DT_GameModeTable.DT_GameModeTable'");
+	
+	GameModeDataTable = Cast<UDataTable>(FSoftObjectPath(*GameModeDataTablePath).ResolveObject());
+	
+	if (!GameModeDataTable)
+		GameModeDataTable = Cast<UDataTable>(FSoftObjectPath(*GameModeDataTablePath).TryLoad());
+
+	SetCurrentGameMode(FString("MainMenu"));
+
+	if (GameModeDataTable)
+		UE_LOG(LogTemp, Display, TEXT("UGameModeSubsystem initialised successfully"))
+}
+
+void UGameModeSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+FName UGameModeSubsystem::GetGameModePath(FString GameModeName) const
+{	
+	FMapTableRow* GameModeTableRow = GameModeDataTable->FindRow<FMapTableRow>(FName(GameModeName), "");
+
+	if (!GameModeTableRow)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Map not found"))
+		return FName("");
+	}
+	
+	return GameModeTableRow->Path;
+}
+
+FName UGameModeSubsystem::GetCurrentGameModeDisplayName() const
+{
+	FMapTableRow* GameModeTableRow = GameModeDataTable->FindRow<FMapTableRow>(CurrentGameMode, "");
+
+	if (!GameModeTableRow)
+		return FName("");
+	
+	return FName(GameModeTableRow->DisplayName);
+}
+
+void UGameModeSubsystem::SetCurrentGameMode(const FString& GameModeName)
+{
+	UE_LOG(LogTemp, Warning, TEXT("LoadingMapString: %s"), *GameModeName)
+	
+	for (const FName& RowName : GameModeDataTable->GetRowNames())
+	{
+		FMapTableRow* GameModeTableRow = GameModeDataTable->FindRow<FMapTableRow>(RowName, "");
+
+		if (GameModeTableRow->Path.ToString() == GameModeName)
+		{
+			CurrentGameMode = RowName;
+			return;
+		}
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("UGameModeSubsystem: Map loading map not found in map table!"))
+}
---
+++ Public/Subsystem/GameModeSubsystem.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MapSubsystem.generated.h"
+
+class ULevelSequence;
+class UDataTable;
+
+
+/**
+ * 
+ */
+UCLASS()
+class POLYRACINGGAME_API UGameModeSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+public:
+
+	UPROPERTY()
+	UDataTable* GameModeDataTable;
+
+	UPROPERTY()
+	FName CurrentGameMode;
+
+	FName GetGameModePath(FString GameModeName) const;
+
+	FName GetCurrentGameModeDisplayName() const;
+
+	void SetCurrentGameMode(const FString& GameModeName);
+};
