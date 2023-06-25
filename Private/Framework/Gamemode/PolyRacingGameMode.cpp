31ece9766beeb10478c6454c8b0da93e4106cf3e
Georgefwm <georgemclachlan2@gmail.com>
2023-06-25 15:51:38 +1200

    Add base class for more literal game modes to dereive from

---
+++ Private/Framework/Gamemode/PolyRacingGameMode.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "..\..\..\Public\Framework\GameMode\PolyRacingGameModeBase.h"
+
+
+// Sets default values
+APolyRacingGameMode::APolyRacingGameMode()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APolyRacingGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APolyRacingGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
