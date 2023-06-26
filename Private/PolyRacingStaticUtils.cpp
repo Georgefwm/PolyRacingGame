5171271e07088a15405bf9b1b8ff2c96ed3ab59d
Georgefwm <georgemclachlan2@gmail.com>
2023-06-27 11:58:05 +1200

    Add static utils class

---
+++ Private/PolyRacingStaticUtils.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PolyRacingStaticUtils.h"
+
+
+static FString GetFloatAsStringWithPrecision(float TheFloat, int32 Precision, bool IncludeLeadingZero)
+{
+	//Round to integral if have something like 1.9999 within precision
+	float Rounded = roundf(TheFloat);
+	
+	if(FMath::Abs(TheFloat - Rounded) < FMath::Pow(10,-1 * Precision))
+	{       
+		TheFloat = Rounded;
+	}
+	
+	FNumberFormattingOptions NumberFormat;					//Text.h
+	NumberFormat.MinimumIntegralDigits = (IncludeLeadingZero) ? 1 : 0;
+	NumberFormat.MaximumIntegralDigits = 10000;
+	NumberFormat.MinimumFractionalDigits = Precision;
+	NumberFormat.MaximumFractionalDigits = Precision;
+	
+	return FText::AsNumber(TheFloat, &NumberFormat).ToString(); 
+}
+
+static FText GetFloatAsTextWithPrecision(float TheFloat, int32 Precision, bool IncludeLeadingZero)
+{
+	//Round to integral if have something like 1.9999 within precision
+	float Rounded = roundf(TheFloat);
+	
+	if(FMath::Abs(TheFloat - Rounded) < FMath::Pow(10,-1 * Precision))
+	{       
+		TheFloat = Rounded;
+	}
+	
+	FNumberFormattingOptions NumberFormat;					//Text.h
+	NumberFormat.MinimumIntegralDigits = (IncludeLeadingZero) ? 1 : 0;
+	NumberFormat.MaximumIntegralDigits = 10000;
+	NumberFormat.MinimumFractionalDigits = Precision;
+	NumberFormat.MaximumFractionalDigits = Precision;
+	
+	return FText::AsNumber(TheFloat, &NumberFormat); 
+}
---
+++ Public/PolyRacingStaticUtils.h
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PolyRacingStaticUtils.generated.h"
+
+
+static FString GetFloatAsStringWithPrecision(float TheFloat, int32 Precision, bool IncludeLeadingZero=true);
+
+static FText GetFloatAsTextWithPrecision(float TheFloat, int32 Precision, bool IncludeLeadingZero=true);
