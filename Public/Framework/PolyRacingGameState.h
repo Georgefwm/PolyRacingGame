a4cedc0b1babccfe7af0456b1566fafc6eb14440
Georgefwm <georgemclachlan2@gmail.com>
2023-06-27 17:03:04 +1200

    Add game state class

---
+++ Public/Framework/PolyRacingGameState.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "PolyRacingGameState.generated.h"
+
+UCLASS()
+class POLYRACINGGAME_API APolyRacingGameState : public AGameState
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APolyRacingGameState();
+
+	UPROPERTY()
+	int LapCount = 3;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
