70b9c6616a38ba8f64ebacaba58282b0718fa86c
Georgefwm <georgemclachlan2@gmail.com>
2023-06-30 19:02:43 +1200

    Fix bug caused by missing include

--- Private/Framework/Gamemode/RaceGameMode.cpp
+++ Private/Framework/Gamemode/RaceGameMode.cpp
@@ -1,6 +1,8 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Framework/GameMode/RaceGameMode.h"
+
+#include "CheckpointActor.h"
 #include "Controller/PolyRacingPlayerController.h"
 #include "Framework/PolyRacingPlayerState.h"
 
