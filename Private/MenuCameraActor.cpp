d28895cce78d3a256937b18ffb3a9fc9f976526c
Georgefwm <georgemclachlan2@gmail.com>
2023-07-11 16:14:04 +1200

    Add custom camera class

---
+++ Private/MenuCameraActor.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MenuCameraActor.h"
+#include "Components/AudioComponent.h"
+
+
+// Sets default values
+AMenuCameraActor::AMenuCameraActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+	AudioComponent->bAutoActivate = false;
+	AudioComponent->bIsMusic = true;
+	AudioComponent->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void AMenuCameraActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AMenuCameraActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AMenuCameraActor::PossessedBy(APlayerController* NewController)
+{
+	if(!AudioComponent->GetSound())
+		return;
+	
+	AudioComponent->Play();
+}
+
---
+++ Public/MenuCameraActor.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "MenuCameraActor.generated.h"
+
+class AMenuPlayerController;
+class USoundCue;
+
+
+UCLASS()
+class POLYRACINGGAME_API AMenuCameraActor : public ACameraActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMenuCameraActor();
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UAudioComponent* AudioComponent;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void PossessedBy(APlayerController* NewController);
+};
